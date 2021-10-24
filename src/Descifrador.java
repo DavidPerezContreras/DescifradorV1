f82c9c5517d6a6959200dbd85b16b6744a5c5cc7
David Pérez Contreras <david.perez.dam2@gmail.com>
2021-10-24 14:29:37 +0200

    FUNCIONA, arreglado algun fallo, también con archivos sin extensión.

--- src/Descifrador.java
+++ src/Descifrador.java
@@ -16,90 +16,87 @@ public class Descifrador {
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         };
 
-        if (args.length == 2 ) {//Hacen falta 2 argumentos , da tambien funciona con archivos sin extensión
+        if (args.length == 2 ) {//Hacen falta 2 argumentos ,
 
-                //Argumentos
-                final String nombreDelFichero = args[0];    //Nombre del fichero que hay que descifrar
-                final int key = Integer.parseInt(args[1]);  //Clave
-
-
-                //Separa el nombre del fichero de la extensión.   archivo  .txt. Si no tiene extensión, también funciona
-                final String nombreDelFicheroSinExtension = nombreDelFichero.contains(".")?nombreDelFichero.substring(0, nombreDelFichero.lastIndexOf(".")):"";
-                final String extensionDelFichero = nombreDelFichero.contains(".")?nombreDelFichero.substring(nombreDelFichero.lastIndexOf(".")):"";
+            //Argumentos
+            final String nombreDelFichero = args[0];    //Nombre del fichero que hay que descifrar
+            final int key = Integer.parseInt(args[1]);  //Clave
 
 
-                //Archivo que hay que descifrar
-                final File archivo1 = new File("src" + slash + "datos" + slash + nombreDelFichero);
-                //Archivo nuevo que almacena el resultado
-                final File archivoNuevo1 = nombreDelFichero.contains(".")?new File("src" + slash + "datos" + slash + nombreDelFicheroSinExtension + ".nuevo" + extensionDelFichero):new File("src" + slash + "datos" + slash + nombreDelFichero + ".nuevo" );
+            //Separa el nombre del fichero de la extensión.   archivo  .txt. Si no tiene extensión, también funciona
+            final String nombreDelFicheroSinExtension = nombreDelFichero.contains(".")?nombreDelFichero.substring(0, nombreDelFichero.lastIndexOf(".")):"";
+            final String extensionDelFichero = nombreDelFichero.contains(".")?nombreDelFichero.substring(nombreDelFichero.lastIndexOf(".")):"";
 
 
-                if (archivo1.exists() && archivoNuevo1.exists() ) {
+            //Archivo que hay que descifrar
+            final File archivo1 = new File("src" + slash + "datos" + slash + nombreDelFichero);
+            //Archivo nuevo que almacena el resultado
+            final File archivoNuevo1 = nombreDelFichero.contains(".")?new File("src" + slash + "datos" + slash + nombreDelFicheroSinExtension + ".nuevo" + extensionDelFichero):new File("src" + slash + "datos" + slash + nombreDelFichero + ".nuevo" );
 
-                    try {
-                        FileReader frArchivo1 = new FileReader(archivo1);
-                        FileWriter fwArchivoNuevo1 = new FileWriter(archivoNuevo1);
+
+            if (archivo1.exists() && archivoNuevo1.exists() ) {//Comprueba que los archivos con los que va a trabajar existen (Si archivoNuevo1 no existe, tampoco lo crea
+
+                try {
+                    FileReader frArchivo1 = new FileReader(archivo1);
+                    FileWriter fwArchivoNuevo1 = new FileWriter(archivoNuevo1);
 
 
-                        int characterSelected = frArchivo1.read();
-                        boolean isLetter;//Almacena en cada vuelta del siguiente while si el character que maneja es una letra o no
-                        while (characterSelected != -1) {
-                            isLetter = false;
-                            //Para cada caracter del archivo:
-                            //
-                            for (int i = 0; i < arrayAbecedarioMayuscula.length; i++) {
+                    int characterSelected = frArchivo1.read();
+                    boolean isLetter;//Almacena en cada vuelta del siguiente while si el character que maneja es una letra o no
+                    while (characterSelected != -1) {
+                        isLetter = false;
+                        //Para cada caracter del archivo:
+                        if (key < 0) { //Si key es mayor que cero cifra
+                            for (int i = 0; i < arrayAbecedarioMayuscula.length; i++) {     //Recorre el abecedario en Mayuscula y si coincide lo escribe en archivoNuevo1
+                                if (arrayAbecedarioMayuscula[i] == (char) characterSelected) {
+                                    fwArchivoNuevo1.write(arrayAbecedarioMayuscula[(i + key) % arrayAbecedarioMayuscula.length]);
+                                    isLetter = true;
+                                }
+                                if (arrayAbecedarioMinuscula[i] == (char) characterSelected) {
+                                    fwArchivoNuevo1.write(arrayAbecedarioMinuscula[(i + key) % arrayAbecedarioMinuscula.length]);
+                                    isLetter = true;
+                                }
 
-                                if (key < 0) {
-                                    if (arrayAbecedarioMayuscula[i] == (char) characterSelected) {
-                                        fwArchivoNuevo1.write(arrayAbecedarioMayuscula[(i + key) % arrayAbecedarioMayuscula.length]);
-                                        isLetter = true;
-                                    }
+                            }
 
+
+                        }
+
+                        if (key > 0) {                          //Si key es mayor que cero cifra
+                            for (int i = 0; i < arrayAbecedarioMinuscula.length; i++) {
+                                if (key > 0) {
                                     if (arrayAbecedarioMinuscula[i] == (char) characterSelected) {
                                         fwArchivoNuevo1.write(arrayAbecedarioMinuscula[(i + key) % arrayAbecedarioMinuscula.length]);
                                         isLetter = true;
                                     }
-
-                                }
-
-
-                            }
-
-                            if (key > 0) {
-                                for (int i = 0; i < arrayAbecedarioMinuscula.length; i++) {
-                                    if (key > 0) {
-                                        if (arrayAbecedarioMinuscula[i] == (char) characterSelected) {
-                                            fwArchivoNuevo1.write(arrayAbecedarioMinuscula[(i + key) % arrayAbecedarioMinuscula.length]);
-                                            isLetter = true;
-                                        }
-                                        if (arrayAbecedarioMayuscula[i] == (char) characterSelected) {
-                                            fwArchivoNuevo1.write(arrayAbecedarioMayuscula[(i + key) % arrayAbecedarioMayuscula.length]);
-                                            isLetter = true;
-                                        }
+                                    if (arrayAbecedarioMayuscula[i] == (char) characterSelected) {
+                                        fwArchivoNuevo1.write(arrayAbecedarioMayuscula[(i + key) % arrayAbecedarioMayuscula.length]);
+                                        isLetter = true;
                                     }
                                 }
                             }
+                        }
 
 
-                            if (!isLetter || key == 0) {//Si no es una letra imprime el carácter directamente
-                                fwArchivoNuevo1.write((char) characterSelected);
-                            }
-                            characterSelected = frArchivo1.read();
-                        }//FIN DEL WHILE
+                        if (!isLetter || key == 0) {//Si no es una letra imprime el carácter directamente
+                            fwArchivoNuevo1.write((char) characterSelected);
+                        }
+                        characterSelected = frArchivo1.read();
+                    }//FIN DEL WHILE
 
-                        //! Cierra el archivo
-                        frArchivo1.close();
+                    //! Cierra el archivo
+                    frArchivo1.close();
 
 
-                        fwArchivoNuevo1.close();    //Cierra el FileWriter y se escriben los cambios en el fichero.
-                    } catch (IOException e) {//Fin del try
-                        e.printStackTrace();
-                    }
-                } else {
-                    System.out.println("No se ha encontrado el archivo!");
+                    fwArchivoNuevo1.close();    //Cierra el FileWriter y se escriben los cambios en el fichero.
+                } catch (IOException e) {//Fin del try
+                    e.printStackTrace();
                 }
+            } else {
+                System.out.println("No se ha encontrado el archivo!");
+            }
 
-            }else{System.out.println("Debes proporcionar 2 argumentos");}
-    }
+        }else{System.out.println("Debes proporcionar 2 argumentos");}
+    }//Fin del metodo main
 
-}
+}//Fin de la clase
